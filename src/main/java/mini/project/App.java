caee3e5bf3a7319f9c251e0981b71322065d48fa
cgkim449 <cgkim449@gmail.com>
2020-09-09 09:55:10 +0900

    테스트

--- src/main/java/mini/project/App.java
+++ src/main/java/mini/project/App.java
@@ -9,7 +9,6 @@ public class App {
   }
 
   public static void main(String[] args) {
-    System.out.println(new App().getGreeting());
-    System.out.println("김찬구!");
+    System.out.println("2020-09-09");
   }
 }
