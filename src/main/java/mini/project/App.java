8dab2f50a4e756e1a4707711d358479fe2726211
jiyounyou <younny418@gmail.com>
2020-09-09 14:54:36 +0900

    .

--- src/main/java/mini/project/App.java
+++ src/main/java/mini/project/App.java
@@ -11,5 +11,7 @@ public class App {
   public static void main(String[] args) {
     System.out.println("안녕안녕?!!");
     System.out.println("2020-09-09-09-09-09-09");
+    System.out.println("어휴 배고파");
+    System.out.println("점심 뭐먹지");
   }
 }
